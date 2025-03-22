1bf2e315d8dca38f234d3b712f0fb6d8e940fe20
rahyanakil <rahyanakil89@gmail.com>
2025-03-23 01:46:55 +0600

    task controller setup done

--- app/controllers/TaskController.js
+++ app/controllers/TaskController.js
@@ -1,1 +1,29 @@
- 
+export const CreateTask = async (req, res) => {
+  return res
+    .status(200)
+    .json({ status: "success", Message: "CreateTask Successfull" });
+};
+
+export const UpdateTaskStatus = async (req, res) => {
+  return res
+    .status(200)
+    .json({ status: "success", Message: "UpdateTaskStatus Successfull" });
+};
+
+export const TaskListByStatus = async (req, res) => {
+  return res
+    .status(200)
+    .json({ status: "success", Message: "TaskListByStatus Successfull" });
+};
+
+export const DeleteTask = async (req, res) => {
+  return res
+    .status(200)
+    .json({ status: "success", Message: "DeleteTask Successfull" });
+};
+
+export const CountTask = async (req, res) => {
+  return res
+    .status(200)
+    .json({ status: "success", Message: "CountTask Successfull" });
+};
